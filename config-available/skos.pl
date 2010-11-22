e5a90381821cbe75d314eb6ea91e645b88d8a359
Jan Wielemaker <J.Wielemaker@cs.vu.nl>
2010-11-22 16:43:02 +0100

    Added SKOS files

--- config-available/skos.pl
+++ config-available/skos.pl
@@ -1,5 +1,13 @@
 :- module(conf_skos, []).
+:- use_module(library(semweb/rdf_db)).
+:- use_module(library(semweb/rdf_library)).
 
-/** <module> SKOS schema and libraries
+/** <module> Provide SKOS schema and namespace
+
+This module provides the SKOS schema and   the  prefix =skos= for use in
+Prolog.
 */
 
+:- rdf_register_ns(foaf, 'http://www.w3.org/2004/02/skos/core#').
+:- rdf_attach_library(skos(rdf)).
+:- rdf_load_library(skos).
