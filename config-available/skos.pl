045008a939c46f869caac39919cdddfec3c3c87c
Jan Wielemaker <J.Wielemaker@cs.vu.nl>
2010-11-23 14:16:04 +0100

    Added manifest; fixed namespace

--- config-available/skos.pl
+++ config-available/skos.pl
@@ -8,6 +8,6 @@ This module provides the SKOS schema and   the  prefix =skos= for use in
 Prolog.
 */
 
-:- rdf_register_ns(foaf, 'http://www.w3.org/2004/02/skos/core#').
+:- rdf_register_ns(skos, 'http://www.w3.org/2004/02/skos/core#').
 :- rdf_attach_library(skos(rdf)).
 :- rdf_load_library(skos).
