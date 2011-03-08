11140eac65dad0de9e83330f2fa5b3137281a537
Jan Wielemaker <J.Wielemaker@cs.vu.nl>
2011-03-08 11:51:13 +0100

    Removed most of SKOS component handling, only leaving the code for finding SKOS-XL labels.

--- config-available/skos.pl
+++ config-available/skos.pl
@@ -10,15 +10,8 @@ This module provides hooks for showing SKOS entities
 :- use_module(components(skos/components)).
 :- use_module(components(skos/graph)).
 
-cliopatria:display_link(R, Options) -->
-	skos_display_link(R, Options).
-
 cliopatria:context_graph(R, RDF, Options) :-
 	skos_context_graph(R, RDF, Options).
 
 cliopatria:node_shape(URI, Shape, Options) :-
 	skos_node_shape(URI, Shape, Options).
-
-cliopatria:list_resource(SKOS) -->
-	{ rdfs_individual_of(SKOS, skos:'Concept') },
-	skos_concept_view(SKOS, []).
